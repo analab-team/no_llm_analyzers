9f46a126e3bf6fc98f89ea9ec7e97fbc55399093
Mark Basov <basovmark@gmail.com>
2024-09-13 00:32:44 +0300

    upd commit

---
+++ banword_analyzer/app/core/config/__init__.py
@@ -0,0 +1,5 @@
+from core.config.config_loader import main_config
+from pathlib import Path
+import os
+
+PROJECT_PATH = Path(os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../')))
---
+++ config/__init__.py
@@ -0,0 +1,4 @@
+import os
+from pathlib import Path
+
+PROJECT_PATH = Path(os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../')))
